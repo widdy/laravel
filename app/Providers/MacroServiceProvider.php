ace4e99269fc0d0149141d10717e471684bce43d
chastephp <chastephp@foxmail.com>
2020-09-10 16:51:23 +0800

    在json响应中处理Paginator

--- app/Providers/MacroServiceProvider.php
+++ app/Providers/MacroServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\JsonResponse;
+use Illuminate\Pagination\AbstractPaginator;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\ServiceProvider;
 
@@ -18,66 +21,80 @@ class MacroServiceProvider extends ServiceProvider
          * success
          * @param  $msg string
          * @param  $data mixed
-         * @return \Illuminate\Http\JsonResponse
+         * @return JsonResponse
          */
         Response::macro('success', function (string $msg = 'success', $data = null) {
             $ret = ['code' => 0, 'message' => $msg, 'data' => $data];
-            if (!is_string($msg)) {
-                list($data, $msg) = [$msg, $data ?? 'success'];
+
+            if ($ret['data'] instanceof AbstractPaginator) {
+                $ret['data'] = Arr::only($ret['data']->toArray(), ['data', 'current_page', 'per_page', "total"]);
             }
+
             if ($ret['data'] === null) {
                 unset($ret['data']);
             }
 
-            return Response::json($ret,
-                200, ['Content-Type' => 'application/json; charset=utf-8'], JSON_UNESCAPED_UNICODE);
+            return Response::json($ret, 200, [], JSON_UNESCAPED_UNICODE);
         });
 
         /**
          * error
          * @param  $msg string
          * @param  $data mixed
-         * @return \Illuminate\Http\JsonResponse
+         * @return JsonResponse
          */
         Response::macro('error', function (string $msg = 'error', $data = null) {
             $ret = ['code' => 1, 'message' => $msg, 'data' => $data];
-            if ($data === null) {
+
+            if ($ret['data'] instanceof AbstractPaginator) {
+                $ret['data'] = Arr::only($ret['data']->toArray(), ['data', 'current_page', 'per_page', "total"]);
+            }
+
+            if ($ret['data'] === null) {
                 unset($ret['data']);
             }
-            return Response::json($ret,
-                200, ['Content-Type' => 'application/json; charset=utf-8'], JSON_UNESCAPED_UNICODE);
+
+            return Response::json($ret, 200, [], JSON_UNESCAPED_UNICODE);
         });
 
         /**
          * system error, illegal request ...
          * @param  $msg string
          * @param  $data mixed
-         * @return \Illuminate\Http\JsonResponse
+         * @return JsonResponse
          */
         Response::macro('bad', function (string $msg = 'service unavailable ', $data = null) {
-            $ret = ['code' => 1, 'message' => $msg, 'data' => $data];
-            if ($data === null || !config('app.debug')) { //data is null or debug is false
+            $ret = ['code' => -1, 'message' => $msg, 'data' => $data];
+
+            if ($ret['data'] instanceof AbstractPaginator) {
+                $ret['data'] = Arr::only($ret['data']->toArray(), ['data', 'current_page', 'per_page', "total"]);
+            }
+
+            if ($ret['data'] === null) {
                 unset($ret['data']);
             }
-            return Response::json($ret,
-                200, ['Content-Type' => 'application/json; charset=utf-8'], JSON_UNESCAPED_UNICODE);
+
+            return Response::json($ret, 200, [], JSON_UNESCAPED_UNICODE);
         });
 
         /**
          * unauthenticated or token expired
          * @param  $msg string
          * @param  $data mixed
-         * @return \Illuminate\Http\JsonResponse
+         * @return JsonResponse
          */
         Response::macro('unauthenticated', function (string $msg = 'unauthenticated', $data = null) {
-
             $ret = ['code' => 40001, 'message' => $msg, 'data' => $data];
-            if ($data === null || !config('app.debug')) { //data is null or debug is false
+
+            if ($ret['data'] instanceof AbstractPaginator) {
+                $ret['data'] = Arr::only($ret['data']->toArray(), ['data', 'current_page', 'per_page', "total"]);
+            }
+
+            if ($ret['data'] === null) {
                 unset($ret['data']);
             }
-            return Response::json($ret,
-                200, ['Content-Type' => 'application/json; charset=utf-8'], JSON_UNESCAPED_UNICODE);
 
+            return Response::json($ret, 200, [], JSON_UNESCAPED_UNICODE);
         });
     }
 }
